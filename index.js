65ed4a7bec4b33133d06577a200b5a47a9be7f40
Aarish Duwal <Aarishduwal123@gmail.com>
2026-06-03 02:22:09 +0545

    Cred

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function buildRegisletEmbed(r) {
         inline: false,
       }
     )
-    .setFooter({ text: 'Regislet Guide  •  /regislet  /regislet_location  /regislet_list  •  Data Credits: Coryn.Club' })
+    .setFooter({ text: 'Regislet Guide  •  /regislet_help  •  Data Credits: Coryn.Club' })
     .setTimestamp();
 
   return embed;
